165c65240dcaf0eadcc84aff3c6bcb2a2f38c049
carloshdzcasares <chcandristudio@gmail.com>
2021-09-24 00:12:14 +0200

    finish method

--- streams/src/com/chc/Main.java
+++ streams/src/com/chc/Main.java
@@ -1,15 +1,16 @@
 package com.chc;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
 
     private static List<User> users;
+    private static Optional<List<Boolean>> actives;
+
     public static void main(String[] args) {
 	// Un stream es un conjunto de funciones que se ejecutan de forma anidada , las más conocidas nos permiten reducir hacer , map
         // son envoltorios de una fuente de datos , no almacena datos.
@@ -78,7 +79,9 @@ public class Main {
         En el stream tenemos métodos intermedios , es decir que acontinuación de ellos puede seguir habiendo más métodos
         Collect es un método final , forEach ... para verlo se puede comprobar poniendo un punto al final.
         Con Peek podemos recorrer la lista y seguir haciendo operaciones
+
      */
+
         System.out.println("--------------------------Peek-----------------------------");
         setUpUser();
 
@@ -123,14 +126,188 @@ public class Main {
                 .sorted(Comparator.comparing(User::getNombre))
                 .collect(Collectors.toList());
         imprimirLista();
+        /*
+        Min Max : Nos permite obtener el valor mínimo o el valor máximo de la lista
+        */
+        System.out.println("--------------------------Min Max-----------------------------");
+        setUpUser();
+        User userMin = users.stream()
+                .min(Comparator.comparing(User::getId))
+                .orElse(null);
+        System.out.println(userMin.getId());
+        User userMax = users.stream()
+                .min(Comparator.comparing(User::getId))
+                .orElse(null);
+        System.out.println(userMax.getId());
+        /*
+        Distinct : Nos permite eliminar los valores repetidos
+        */
+        System.out.println("--------------------------Distinct-----------------------------");
+        String[] abce = {"a","b","c","a","b","c","e","f","g","h","i","j","k","..."};
+        List<String> abcFilter1 = Arrays.stream(abce).distinct().collect(Collectors.toList());
+        abcFilter1.forEach(System.out::println);
+        System.out.println(Arrays.asList(abce));
 
         /*
+        AllMatch, anyMatch, noneMatch :  AllMatch verifica si el predicado es verdadero,
+         verifica si un valor es verdadero y nonematch verifica si ninguno pasa el predicado
+
+        */
+        System.out.println("--------------------------AllMatch, anyMatch, noneMatch-----------------------------");
+        List<Integer> listaNumeros = Arrays.asList(100,300,900,5000);
+
+        //Todos  superan 300
+        boolean allMatch = listaNumeros.stream().allMatch(e->e>300);
+        System.out.println(allMatch);
+        //Al menos uno supera 301
+        boolean anyMatch = listaNumeros.stream().anyMatch(e->e>301);
+        System.out.println(anyMatch);
+        //Ninguno supera los 10000
+        boolean noneMatch = listaNumeros.stream().noneMatch(e->e>300);
+        System.out.println(noneMatch);
+
+        /*
+        SuM averange range: Nos va a permitir sumar , la media y el range para hacer la implentación en elementos
+
+        */
+        System.out.println("--------------------------SuM averange range-----------------------------");
+        setUpUser();
+        double result = users.stream()
+                .mapToInt(User::getId)
+                .average()
+                .orElse(0);
+        System.out.println(result);
+
+        result = users.stream()
+                .mapToInt(User::getId)
+                .sum();
+        System.out.println(result);
+//Nos proporciona un rango y luego hacemos la suma de ese rango
+        System.out.println(IntStream.range(0,100).sum());
+
+                /*
+        Reduce: Fol , toma el string y los combina en un unico resultado mediante la acción repetida de una aplicación
+
+        */
+        System.out.println("--------------------------Reduce-----------------------------");
+        setUpUser();
+        int numero = users.stream()
+                .map(User::getId)
+                .reduce(0, Integer::sum);
+        System.out.println(numero);
+
+                        /*
+        Joining: devuelve un recompilador que contatena la secuenca de charsecuency y lo devuelve como una cadena
+        Nos llega como una lista de nombre de usuarios y los mostramos como un único string separado por un guión
+
+        */
+        System.out.println("--------------------------Joining-----------------------------");
+        setUpUser();
+        String names = users.stream()
+                .map(User::getNombre)
+                .collect(Collectors.joining(" - "));
+        System.out.println(names);
+
+        /*
+        toSet: Nos devuelve un collector que acumula los elementos de entrada en un nuevo set
+
+        */
+        System.out.println("--------------------------toSet-----------------------------");
+        setUpUser();
+        Set<String> setNames = users.stream()
+                .map(User::getNombre)
+                .collect(Collectors.toSet());
+        setNames.forEach(System.out::println);
+        /*
+        sumarizingDouble: Nos devuelve estadisticas muy interesantes en una variable doubleSumaryStatistics
+
+        */
+        System.out.println("--------------------------sumarizingDouble-----------------------------");
+        setUpUser();
+        DoubleSummaryStatistics statistics = users.stream()
+                .collect(Collectors.summarizingDouble(User::getId));
+        System.out.println(statistics.getAverage() + " " +
+                                   statistics.getMax() + " " +
+                                   statistics.getMin() + " " +
+                                   statistics.getCount() + " " +
+                                   statistics.getSum());
+        //Segunda forma de hacerlo
+        DoubleSummaryStatistics statistics1 = users.stream()
+                .mapToDouble(User::getId)
+                .summaryStatistics();
+        System.out.println(statistics1.getAverage() + " " +
+                                   statistics1.getMax() + " " +
+                                   statistics1.getMin() + " " +
+                                   statistics1.getCount() + " " +
+                                   statistics1.getSum());
+
+        /*
+        PartitioningBy:  Nos va a devolver nuestra lista de elementos divida en 2 en una que se cumpla el predicado
+        y otra en la que no
+        Imaginamos que tenemos 2 salarios distiontos de salarios por directivos.
+
+        */
+        System.out.println("--------------------------PartitioningBy-----------------------------");
+        setUpUser();
+        List<Integer> numeros = Arrays.asList(5,7,8,6,2,1,4,7,3123);
+        Map<Boolean, List<Integer>> esMayor = numeros.stream()
+                .collect(Collectors.partitioningBy(e ->e>10));
+        esMayor.get(true).forEach(System.out::println);
+        esMayor.get(false).forEach(System.out::println);
+        /*
+        groupingBy: Agrupamos según queramos , se debe indicar la propiedad por la cual se debe indicar la agrupación
+
+        */
+        System.out.println("--------------------------groupingBy-----------------------------");
+        setUpUser();
+        Map<Character, List<User>> grupoAlfabetico = users.stream()
+                //Aquí es donde indicamos como queremos agrupar y los criterios.
+                .collect(Collectors.groupingBy(e -> e.getNombre().charAt(0)));
+        grupoAlfabetico.get('J').forEach(e -> System.out.println(e.getNombre()));
+        grupoAlfabetico.get('A').forEach(e -> System.out.println(e.getNombre()));
+        grupoAlfabetico.get('C').forEach(e -> System.out.println(e.getNombre()));
+        /*
+        mapping:
+        */
+        System.out.println("--------------------------mapping-----------------------------");
+        setUpUser();
+        List<String> personas = users.stream()
+                .map(User::getNombre)
+                .collect(Collectors.toList());
+        personas.forEach(System.out::println);
+        /*
+        stream paralelo: Es la forma de utilizar el stream por diferentes hilos para reducir los tiempos de ejecución
+        Se reducen los tiempos de ejcución casi en 5 partes ... es un 400% más rápido.
+        Puede ser muy útil  cuando tienes que hacer operaciones largas para reducir el tiempo
+        */
+        System.out.println("--------------------------stream paralelo-----------------------------");
+        setUpUser();
+        long tiempos = System.currentTimeMillis();
+        listaString.forEach(Main::convertirAMayusculas);
+        long tiempos2 = System.currentTimeMillis();
+        System.out.println("Normal " + (tiempos-tiempos2));
+        tiempos = System.currentTimeMillis();
+        listaString.parallelStream().forEach(Main::convertirAMayusculas);
+        tiempos2 = System.currentTimeMillis();
+        System.out.println("Paralelo: " + (tiempos-tiempos2));
+
+
+        /*
+        Cuando tengas que hacer algo con una lista , casi seguro que un stream pueda resolverlo por tí
         Recapitulación sobre los Streams :
             Tenemos una fuente de datos : Una lista , un collection , un array etc ...
             Através del stream indicamos unas operaciones intermedias como filter, map, peek..
             Por último siempre tendremos una operación final para devolver el tipo de dato que hemos declarado.
          */
 
+    }
+    private static String convertirAMayusculas ( String nombre){
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return nombre.toUpperCase();
     }
 
     private static void setUpUser(){
@@ -147,4 +324,24 @@ public class Main {
     private static void imprimirLista(){
         users.stream().forEach(e -> System.out.println(e.getId() + " " + e.getNombre()));
     }
+
+/*    //Proponer para WebStarter , o cambiar a boolean normal , además de mirar como hacerlo con una lambda
+    public static Optional<Boolean> decide(Optional<List<Boolean>> actives) {
+
+
+        return actives.map(multiplesActive -> {
+            if(multiplesActive.containsAll(Arrays.asList(true, false))) {
+                return null;
+            }
+            else if(multiplesActive.contains(true)) {
+                return Boolean.TRUE;
+            }
+            else if(multiplesActive.contains(false)) {
+                    return Boolean.FALSE;
+                }
+                else {
+                    return null;
+                }
+        });
+    }*/
 }
